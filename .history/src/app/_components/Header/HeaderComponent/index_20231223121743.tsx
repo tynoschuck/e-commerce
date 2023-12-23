e676ccad5789fc5fe9ddc5da53d2dbef8724f0f4
tynoschuck <58395745+tynoschuck@users.noreply.github.com>
2023-12-23 12:30:30 +0000

    first commit

---
+++ .history/src/app/_components/Header/HeaderComponent/index_20231223121743.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+
+  return (
+    <nav
+      className={classes.header}>
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+        </Link>
+
+        <HeaderNav header={header} />
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
