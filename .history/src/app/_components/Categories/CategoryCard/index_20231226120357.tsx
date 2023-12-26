bff7721c6622a4a8ba2a1d1cdb51cc9d0d158f82
tynoschuck <58395745+tynoschuck@users.noreply.github.com>
2023-12-26 12:06:46 +0000

    boxing day

--- .history/src/app/_components/Categories/CategoryCard/index_20231226120357.tsx
+++ .history/src/app/_components/Categories/CategoryCard/index_20231226120357.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Link from 'next/link'
 
 import { Category } from '../../../../payload/payload-types'
-import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
 
@@ -13,14 +12,12 @@ type CategoryCardProps = {
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
   const media = category.media as Media
-  const { setCategoryFilters } = useFilter()
 
   return (
     <Link
       href="/products"
       className={classes.card}
       style={{ backgroundImage: `url(${media.url})` }}
-      onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
     </Link>
---
+++ .history/src/app/_providers/index_20231226110207.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import React from 'react'
+
+import { AuthProvider } from '../_providers/Auth'
+import { CartProvider } from '../_providers/Cart'
+import { FilterProvider } from './Filter'
+import { ThemeProvider } from './Theme'
+
+export const Providers: React.FC<{
+  children: React.ReactNode
+}> = ({ children }) => {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <FilterProvider>{children}</FilterProvider>
+        <CartProvider>{children}</CartProvider>
+      </AuthProvider>
+    </ThemeProvider>
+  )
+}
